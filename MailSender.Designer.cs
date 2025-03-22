a2d349c381cafd27526fe8d36621c0886dad2cc9
Abdelrahman Fathy <abdofathy883@gmail.com>
2025-03-22 05:52:04 +0200

    Add project files.

---
+++ MailSender.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Email_Sender
+{
+    partial class MailSender
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            EmailInput = new TextBox();
+            MailFileDialog = new OpenFileDialog();
+            label1 = new Label();
+            SendBtn = new Button();
+            BrowseBtn = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            MailsFromExcel = new ListBox();
+            SuspendLayout();
+            // 
+            // EmailInput
+            // 
+            EmailInput.Location = new Point(38, 95);
+            EmailInput.Name = "EmailInput";
+            EmailInput.Size = new Size(357, 23);
+            EmailInput.TabIndex = 0;
+            // 
+            // MailFileDialog
+            // 
+            MailFileDialog.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label1.AutoSize = true;
+            label1.Location = new Point(38, 68);
+            label1.Name = "label1";
+            label1.Size = new Size(113, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Sen Individual Email";
+            // 
+            // SendBtn
+            // 
+            SendBtn.Location = new Point(38, 220);
+            SendBtn.Name = "SendBtn";
+            SendBtn.Size = new Size(353, 29);
+            SendBtn.TabIndex = 2;
+            SendBtn.Text = "Send";
+            SendBtn.UseVisualStyleBackColor = true;
+            SendBtn.Click += SendBtn_Click;
+            // 
+            // BrowseBtn
+            // 
+            BrowseBtn.Location = new Point(38, 159);
+            BrowseBtn.Name = "BrowseBtn";
+            BrowseBtn.Size = new Size(113, 27);
+            BrowseBtn.TabIndex = 3;
+            BrowseBtn.Text = "Browse";
+            BrowseBtn.UseVisualStyleBackColor = true;
+            BrowseBtn.Click += BrowseBtn_Click;
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label2.AutoSize = true;
+            label2.Location = new Point(38, 138);
+            label2.Name = "label2";
+            label2.Size = new Size(160, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Send Bulk Emails in Excel File";
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 8.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.Red;
+            label3.Location = new Point(38, 189);
+            label3.Name = "label3";
+            label3.Size = new Size(268, 13);
+            label3.TabIndex = 5;
+            label3.Text = "Make sure the file have column with header 'Email'";
+            // 
+            // MailsFromExcel
+            // 
+            MailsFromExcel.FormattingEnabled = true;
+            MailsFromExcel.Location = new Point(426, 95);
+            MailsFromExcel.Name = "MailsFromExcel";
+            MailsFromExcel.Size = new Size(179, 154);
+            MailsFromExcel.TabIndex = 6;
+            // 
+            // MailSender
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(160, 137, 99);
+            ClientSize = new Size(651, 323);
+            Controls.Add(MailsFromExcel);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(BrowseBtn);
+            Controls.Add(SendBtn);
+            Controls.Add(label1);
+            Controls.Add(EmailInput);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "MailSender";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Email Sender";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox EmailInput;
+        private OpenFileDialog MailFileDialog;
+        private Label label1;
+        private Button SendBtn;
+        private Button BrowseBtn;
+        private Label label2;
+        private Label label3;
+        private ListBox MailsFromExcel;
+    }
+}
---
+++ MailSender.cs
@@ -0,0 +1,163 @@
+using ExcelDataReader;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using System.Data;
+using System.Net.Http.Headers;
+using static System.IO.Stream;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+namespace Email_Sender
+{
+    public partial class MailSender : Form
+    {
+        private List<string> EmailList = new List<string>();
+        public MailSender()
+        {
+            InitializeComponent();
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            foreach (Control ctrl in this.Controls)
+            {
+                if (ctrl is Button btn)
+                {
+                    btn.FlatStyle = FlatStyle.Flat;
+                    btn.FlatAppearance.BorderSize = 0;
+                    btn.BackColor = Color.FromArgb(112, 109, 84);
+                }
+            }
+        }
+
+        private List<string> ReadEmailsFromExcel(string FilePath)
+        {
+            var Emails = new List<string>();
+            using (var Stream = File.Open(FilePath, FileMode.Open, FileAccess.Read))
+            {
+                using (var ExcelReader = ExcelDataReader.ExcelReaderFactory.CreateReader(Stream))
+                {
+                    var DataSet = ExcelReader.AsDataSet(new ExcelDataSetConfiguration
+                    {
+                        ConfigureDataTable = _ => new ExcelDataTableConfiguration()
+                        {
+                            UseHeaderRow = true
+                        }
+                    });
+                    var DataTable = DataSet.Tables[0];
+                    var EmailColumn = DataTable.Columns
+                        .Cast<DataColumn>()
+                        .FirstOrDefault(col => col.ColumnName.Trim().Equals("Email", StringComparison.OrdinalIgnoreCase));
+                    if (EmailColumn != null)
+                    {
+
+
+                        foreach (DataRow row in DataTable.Rows)
+                        {
+                            if (DataTable.Columns.Contains("Email"))
+                            {
+                                var email = row["Email"].ToString();
+                                if (!string.IsNullOrEmpty(email))
+                                {
+                                    Emails.Add(email);
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Email column not found in the Excel file.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            return Emails;
+        }
+
+        private async Task SendEmailAsync(string toEmail)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Abdo Fathy", "abdofathy883@gmail.com"));
+            message.To.Add(new MailboxAddress("", toEmail));
+            message.Subject = "Job Application – Full Stack .Net Developer";
+
+            var body = new TextPart("plain")
+            {
+                Text = "Dear Hiring Manager,\n\nI am very interested in opportunities at your company. Please find my CV attached.\n\nBest regards,\nAbdo Fathy"
+            };
+
+            string CVPath = @"D:\Email Sender\Abdo Fathy DotNet.pdf";
+            if (!File.Exists(CVPath))
+            {
+                MessageBox.Show("File path is incorrect");
+                return;
+            }
+            var attachment = new MimePart("application", "pdf")
+            {
+                Content = new MimeContent(File.OpenRead(CVPath), ContentEncoding.Base64),
+                ContentDisposition = new ContentDisposition(ContentDisposition.Attachment),
+                ContentTransferEncoding = ContentEncoding.Base64,
+                FileName = Path.GetFileName(CVPath)
+            };
+
+            var multipart = new Multipart("mixed") { body, attachment };
+            message.Body = multipart;
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+                
+                await client.AuthenticateAsync("abdofathy883@gmail.com", "vmgoqytmkjfvvyln");
+                await client.SendAsync(message);
+                await client.DisconnectAsync(true);
+            }
+        }
+
+
+        private void BrowseBtn_Click(object sender, EventArgs e)
+        {
+            MailFileDialog.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
+            if (MailFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                EmailList = ReadEmailsFromExcel(MailFileDialog.FileName);
+                MailsFromExcel.DataSource = EmailList;
+            }
+        }
+
+        private async void SendBtn_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(EmailInput.Text))
+            {
+                try
+                {
+                    await SendEmailAsync(EmailInput.Text);
+                    MessageBox.Show($"Email Sent to: {EmailInput.Text}");
+
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Failed to send to {EmailInput.Text}: {ex.Message}");
+                }
+            }
+            else if (EmailList != null && EmailList.Count > 0)
+            {
+                int SuccessCount = 0;
+                int FailCount = 0;
+                foreach (var email in EmailList)
+                {
+                    try
+                    {
+                        await SendEmailAsync(email);
+                        await Task.Delay(5000);
+                        SuccessCount++;
+                    }
+                    catch (Exception ex)
+                    {
+                        FailCount++;
+                    }
+                }
+                MessageBox.Show($"Email Sent to: {SuccessCount} Successfully, and failed: {FailCount}");
+            }
+            else
+            {
+                MessageBox.Show("Enter Email or Upload Excel File", "Fail To Send Email", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }            
+        }
+    }
+}
